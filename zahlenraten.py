02eecd33f21271d674506a7a04df515b4b428e07
Nicolas8713 <171wita09@o365.htl-leoben.at>
2018-03-05 16:36:36 +0100

    Von Bis

--- zahlenraten.py
+++ zahlenraten.py
@@ -1,27 +1,31 @@
 #Zahlenraten
 import random
-zufallszahl=(random.randint(1,100))
 titel = "Willkomen beim Zahlratespiel!"
-text = "Bitte versuche meine Zahl zwischen 1 und 100 zu erraten."
-eingabeText= "Bitte gib deine Zahl ein: "
-
 print(titel)
-print (text)
-fertig =False
+
+
+
+von = int(input("Bitte eine ZUfallszahlunterschranke festlegen"))
+bis = int(input(" Bitte die Zufallszahl Oberschranke festlegen"))
+zufallszahl=(random.randint(von , bis))
+text = "Bitte versuche meine Zahl zwischen",von, "und " ,bis, " zu erraten."
+print(text)
+eingabeText= "Bitte gib deine Zahl ein: "
 anzahlderversuche=0;
+fertig =False
+
 while fertig == False:
     zahl = int(input(eingabeText))
     anzahlderversuche=anzahlderversuche+1
     if (zahl == zufallszahl):
         print("gewonnen")
-        
+
         fertig = True
     elif(zahl < zufallszahl):
         print("die gesuchte zahl ist größer")
-        
+
     else:
-        print("die gfesuchte zahl ist kleiner")
-    
+        print("die gesuchte zahl ist kleiner")
+
 print("super, du hast dafür nur ", anzahlderversuche, " Versuche benötigt!")
 print("ende")
-    
